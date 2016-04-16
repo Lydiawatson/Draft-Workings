6aae8de5a521409c508c14b869c8c87e51a80f56
Lydia Watson <lydiawatson6@gmail.com>
2016-04-16 16:39:05 +1200

    minor rearranging

--- www/js/home.js
+++ www/js/home.js
@@ -8,7 +8,6 @@ var geoQuery = geoFireRef.query({
         center: [-36.8436, 174.7669],
         radius: 10.5
     });
-//var genderPrefs = [];
 var currentKey;
 var infoValues;
 var originalPos;
@@ -26,7 +25,42 @@ var filtWindow = document.getElementById("popUpFilter");
 var genderPrefs = [];
 var babyPrefs = [];
 var accessPrefs = null;
+editButton.onclick =  determineEditFunction;
+doneButton.onclick = doneEdit;
+cancFilter.onclick = closeFilters;
+document.getElementById('infoCancel').onclick = closeInfo;
+document.getElementById('uploadButton').onclick = uploadPage;
+var credButton = document.getElementById('creditsButton');
+document.getElementById("filterButton").onclick = getFilters;
+credButton.onclick = openCredits;
 
+
+//functions for the basic loading and filtering of the page
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -36.86803405818809, lng: 174.75977897644043},
+        zoom: 15,
+        //disable default controls and properties of the map
+        streetViewControl: false,
+        mapTypeControl: false,
+        zoomControl: false,
+        disableDoubleClickZoom: true
+    });
+    
+    
+    //if geolocation is working center the map on the user's position
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var initialLocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            map.setCenter(initialLocation);
+        });
+    }
+    
+    getData([], [], null)
+}
 function getData(genderPrefs, babyPrefs, accessPrefs) {
     for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
@@ -133,15 +167,66 @@ function getData(genderPrefs, babyPrefs, accessPrefs) {
         });
     });
 }
+function getFilters() {
+    filtWindow.style.display = 'block';
+    map.setOptions({draggable: false});
+}
+function closeFilters() {
+    /*for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }*/
+   /* var */genderPrefs = [];
+    /*var*/ babyPrefs = [];
 
-
-
-
-
-
-
-editButton.onclick =  determineEditFunction;
     
+    var checkIds = ['maleFilter', 'femaleFilter', 'uniFilter', 'maleBabyFilter', 'femBabyFilter', 'uniBabyFilter'];
+    var fireIds = ['male', 'fem', 'uni', 'mBab', 'fBab', 'uBab'];
+    
+    for (var i=0; i<3; i++) {
+        if(document.getElementById(checkIds[i]).checked) {
+            genderPrefs.push(fireIds[i]);
+        }
+    }
+    for (var i=3; i<6; i++) {
+        if(document.getElementById(checkIds[i]).checked) {
+            babyPrefs.push(fireIds[i]);
+        }
+    }
+    
+
+    /*var*/ accessPrefs = null;
+    
+    if (document.getElementById("wheelchairFilter").checked) {
+        accessPrefs = true;
+    }
+    
+    
+    
+    getData(genderPrefs, babyPrefs, accessPrefs);
+    filtWindow.style.display = 'none';
+    map.setOptions({draggable: true});
+}
+function getAddress(markerPos) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'location': markerPos}, function (results, status) {
+        
+//        gives full address:
+        
+        if (status === google.maps.GeocoderStatus.OK) {
+            document.getElementById("address").innerHTML = results[0].formatted_address;
+        } //consider having error messages here but otherwise nah
+    });
+}
+function closeInfo() {
+    document.getElementById('popUpInfo').style.display = 'none';
+    map.setOptions({draggable: true});
+    if (editing) {
+        cancelEdit();
+        exitEditMode();
+    }
+}
+
+//functions for editing
 function determineEditFunction(){
     
 //    alert(editing);
@@ -158,12 +243,6 @@ function determineEditFunction(){
     }
 //    alert("at end: " +editing)
                             };
-
-
-
-
-
-
 function enterEditMode() {
     editing = true
     addressChanged = false;
@@ -181,10 +260,6 @@ function enterEditMode() {
 
     
 }
-
-
-
-
 function cancelEdit() {
     
     for (var i = 0; i<checkValues.length; i++) {
@@ -193,10 +268,6 @@ function cancelEdit() {
     
     //need to reset the address change
 }
-
-
-doneButton.onclick = doneEdit;
-
 function doneEdit() {
     debugger;
    //asess the filters and change them
@@ -248,77 +319,55 @@ function doneEdit() {
 //    exitEditMode();
         
     }
-
-        
+function exitEditMode() {
+    editing = false
     
-    /*firebaseRef.child("filters/" + currentKey + "/looFilters").set(looFilters, function(error) {
-        if(error) {
-            alert("Something has gone horribly wrong." + error);
-        } else {
-            
-            firebaseRef.child("locations/" + currentKey + "/l").set({
-            0: newMarkerPos.lat(), 1: newMarkerPos.lng()}, function (error) {
-                if(error) {
-                    alert("Something has gone horrible wrong." + error);
-                    closeFilters();
-                } else {
-                    alert("This Loocation has been successfully updated. Thank you for your contribution");
-                    closeFilters();
-                }
-            } )
-            
-            
-        }
-        
-    });
-    */
-//    firebaseRef.child("locations/" + currentKey)
+    doneButton.style.display = "none";
+    editButton.src = "img/editPencil.svg";
+    
+    for (var i = 0; i<checkValues.length; i++) {
+        document.getElementById(checkValues[i]).disabled = true;
+    }
+//    getData(genderPrefs, babyPrefs, accessPrefs);
     
     
     
-
-   
-
-
-
-
-
-
-
-
-/*function enterEditModes() {
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
+    if (changeAddressEntered) {
+        debugger;
+//        /*problem*/changingMarker.setMap(null);
+        changingMarker = null;
+//        getData(genderPrefs, babyPrefs, accessPrefs);
     }
     
-         
-
-    var locations = firebaseRef.child("locations/" + currentKey + "/l");
-    locations.on("value", function(snapshot) {
-        var location = snapshot.val();
-            
-                 
-        marker = new google.maps.Marker({
-            position: {lat: location[0], lng: location[1]},
-            map: map, 
-            draggable: true
-        });
-            
-        markers.push(marker);
-        marker.addListener('dragend',function(event) {
-            alert("seven potatoes");
-            newMarkerPos = marker.position;
-                    
-                   
-//            console.log(markerPos);
-        });
-    });
-}*/
-
-
-
-
-
+    document.getElementById("address").removeEventListener("click", changeAddress);
+}
+function exitLocationEdit() {
+    document.getElementById('popUpInfo').style.display = 'block';
+    document.getElementById("cancLocationEdit").style.display = "none";
+    document.getElementById("confLocationEdit").style.display = "none";
+    map.setOptions({draggable: false});
+    changingMarker.setDraggable(false);
+//    jk
+//    /*problem*/changingMarker.setMap(null);
+    
+}
+function confirmLocationEdit() {
+    //???I want these to execute asynchronously/in order: how??
+    getAddress(changingMarker.position);
+    newLat = changingMarker.position.lat();
+    newLng = changingMarker.position.lng();
+//    /*problem*/changingMarker.setMap(null);
+    exitLocationEdit();
+    //    jk
+//    /*problem*/changingMarker.setMap(null);
+//    addressChanged = true;
+    
+}
+function cancelLocationEdit() {
+    changingMarker.setPosition(originalPos);
+    exitLocationEdit();
+    
+}
 function changeAddress() {
 //    var continueFunction = confirm("Do you wish to change the location of this loo?");
 //    if (continueFunction) {
@@ -354,333 +403,15 @@ function changeAddress() {
         
     }
 }
-function exitEditMode() {
-    editing = false
-    
-    doneButton.style.display = "none";
-    editButton.src = "img/editPencil.svg";
-    
-    for (var i = 0; i<checkValues.length; i++) {
-        document.getElementById(checkValues[i]).disabled = true;
-    }
-//    getData(genderPrefs, babyPrefs, accessPrefs);
-    
-    
-    
-    if (changeAddressEntered) {
-        debugger;
-//        /*problem*/changingMarker.setMap(null);
-        changingMarker = null;
-//        getData(genderPrefs, babyPrefs, accessPrefs);
-    }
-    
-    document.getElementById("address").removeEventListener("click", changeAddress);
-}
-function exitLocationEdit() {
-    document.getElementById('popUpInfo').style.display = 'block';
-    document.getElementById("cancLocationEdit").style.display = "none";
-    document.getElementById("confLocationEdit").style.display = "none";
-    map.setOptions({draggable: false});
-    changingMarker.setDraggable(false);
-//    jk
-//    /*problem*/changingMarker.setMap(null);
-    
-}
-
-
-function confirmLocationEdit() {
-    //???I want these to execute asynchronously/in order: how??
-    getAddress(changingMarker.position);
-    newLat = changingMarker.position.lat();
-    newLng = changingMarker.position.lng();
-//    /*problem*/changingMarker.setMap(null);
-    exitLocationEdit();
-    //    jk
-//    /*problem*/changingMarker.setMap(null);
-//    addressChanged = true;
-    
-}
-
-function cancelLocationEdit() {
-    changingMarker.setPosition(originalPos);
-    exitLocationEdit();
-    
-}
-    
-function schangeAddress() {
-//                console.log(markers);
-    
-    var continueFunction = confirm("Do you wish to change the location of this loo?");
-//         var markerPos;
-
-//    if (continueFunction) {
-        var changedLocation;
-        var oldMarkerPos;
-
-            document.getElementById("cancLocationEdit").onclick = function() {
-                     /*????????oldMarkerPos = [location[0], location[1]];
-                changedLocation = false;
-                marker.setMap(null);*/
-            }
-            
-            
-//        map.setOptions({draggable: true});
-//        document.getElementById('popUpInfo').style.display = 'none';
-//        document.getElementById("cancLocationEdit").style.display = "block";
-//        document.getElementById("confLocationEdit").style.display = "block";
-//        
-        document.getElementById("confLocationEdit").onclick = function() {
-            alert("your new marker position is: "  + newMarkerPos.lat() + " " + newMarkerPos.lng());
-            changedLocation = true;
-            getAddress(newMarkerPos);
-//            document.getElementById('popUpInfo').style.display = 'block';
-//            document.getElementById("cancLocationEdit").style.display = "none";
-//            document.getElementById("confLocationEdit").style.display = "none";
-            
-            
-            
-        }
-             
-             
-             
-//    }
-    
-}
-     
-
-             
-            
- 
- 
- 
- 
- 
- /*
-  document.getElementById("address").onclick = function() {
-//                console.log(markers);
-//                
-                
-                
-                if (editing) {
-                    
-                    
-                    
-                        var continueFunction = confirm("Do you wish to change the location of this loo?");
-    if (continueFunction) {
-  
-        map.setOptions({draggable: true});
-        currentMarker.setDraggable(true);
-        document.getElementById('popUpInfo').style.display = 'none';
-    document.getElementById("cancLocationEdit").style.display = "block";
-    document.getElementById("confLocationEdit").style.display = "block";
-   }
-     
-
-   
-
-//    marker.addListener('dragend',function(event) {
-//        alert("seven potatoes");
-//                markerPos = marker.position;
-//        findAddress();
-////            console.log(markerPos);
-//    });
-    
-                }
-                
-            }*/
- 
- 
- 
- function getAddress(markerPos) {
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({'location': markerPos}, function (results, status) {
-        
-//        gives full address:
-        
-        if (status === google.maps.GeocoderStatus.OK) {
-            document.getElementById("address").innerHTML = results[0].formatted_address;
-        } //consider having error messages here but otherwise nah
-    });
-}
- 
- 
- 
- 
-
-//document.getElementById("address").onclick = changeAddress;
-
-/*function changeAddress() {
-//    var continueFunction = confirm("Do you wish to change the location of this loo?");
-//    if (continueFunction) {
-//        
-//        map.setOptions({draggable: false});
-//        marker.setDraggable(true);
-//        
-//    }
-     document.getElementById('popUpInfo').style.display = 'none';
-    document.getElementById("cancLocationEdit").style.display = "block";
-    document.getElementById("confLocationEdit").style.display = "block";
-//    alert("24 peanuts");
-     map.setOptions({draggable: true});
-    marker.setDraggable(true);
-//    marker.addListener('dragend',function(event) {
-//        alert("seven potatoes");
-//                markerPos = marker.position;
-//        findAddress();
-////            console.log(markerPos);
-//    });
-    
-    
-} */
-
-
-
-
-
-
-//doneButton.onclick = endEdit;
-//function endEdit() {
-//    var looFilters = {};
-//    alert("hi");
-//    var updateRef = firebaseRef.child("filters/" + key);
-//    
-//    doneButton.style.display = 'none';
-//    
-//}
-
-
-
-
-cancFilter.onclick = closeFilters;
-
-
-function closeInfo() {
-    document.getElementById('popUpInfo').style.display = 'none';
-    map.setOptions({draggable: true});
-    if (editing) {
-        cancelEdit();
-        exitEditMode();
-    }
-}
-document.getElementById('infoCancel').onclick = closeInfo;
 
 
 function uploadPage() {
     window.open("upload.html", "_self");
 }
-document.getElementById('uploadButton').onclick = uploadPage;
-
 //function that will eventually dictate what happens when the credits button is clicked
 function openCredits() {
     //random stuff so that I can test the function works
     alert('heart has been clicked');
 }
-var credButton = document.getElementById('creditsButton');
-credButton.onclick = openCredits;
 
 
-
-
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-//        center: {lat: -36.8436, lng: 174.7669},
-        center: {lat: -36.86803405818809, lng: 174.75977897644043},
-        zoom: 15,
-//        zoom: 1,
-//        disable default controls and properties of the map
-//        ???consider disabling all default controls and then enabling what you want (currently none) - might be more efficient?
-        streetViewControl: false,
-        mapTypeControl: false,
-        zoomControl: false,
-//        zoomControlOptions: {
-//            position: google.maps.ControlPosition.TOP_LEFT
-//        }
-        disableDoubleClickZoom: true
-//        ???check with others about turning off POIs
-//        styles: [{featureType: "poi", elementType: "labels", stylers: [{visibility: "off"}]}]
-    });
-    
-    
-    //    if geolocation is working change the center of the map to the user's location
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var initialLocation = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            map.setCenter(initialLocation);
-        });
-    }
-    
-    
-     
-    // Create a draggable circle centered on the map
-//    var circle = new google.maps.Circle({
-//        strokeColor: "#6D3099",
-//        strokeOpacity: 0.7,
-//        strokeWeight: 1,
-//        fillColor: "#B650FF",
-//        fillOpacity: 0.35,
-//        map: map,
-//        center: {lat:-36.8436, lng:174.7669},
-//        radius: 10.5 * 1000,
-//        draggable: false
-//    });
-//    
-    getData([], [], null)
-}
-
-
-
-
-
-/*function getFilters() {
-    filtWindow.style.display = 'block';
-    map.setOptions({draggable: false});
-}
-var filtButton = document.getElementById('filterButton');
-filtButton.onclick = getFilters;*/
-
-document.getElementById("filterButton").onclick = function() {
-    filtWindow.style.display = 'block';
-    map.setOptions({draggable: false});
-}
-
-
-
-function closeFilters() {
-    /*for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
-    }*/
-   /* var */genderPrefs = [];
-    /*var*/ babyPrefs = [];
-
-    
-    var checkIds = ['maleFilter', 'femaleFilter', 'uniFilter', 'maleBabyFilter', 'femBabyFilter', 'uniBabyFilter'];
-    var fireIds = ['male', 'fem', 'uni', 'mBab', 'fBab', 'uBab'];
-    
-    for (var i=0; i<3; i++) {
-        if(document.getElementById(checkIds[i]).checked) {
-            genderPrefs.push(fireIds[i]);
-        }
-    }
-    for (var i=3; i<6; i++) {
-        if(document.getElementById(checkIds[i]).checked) {
-            babyPrefs.push(fireIds[i]);
-        }
-    }
-    
-
-    /*var*/ accessPrefs = null;
-    
-    if (document.getElementById("wheelchairFilter").checked) {
-        accessPrefs = true;
-    }
-    
-    
-    
-    getData(genderPrefs, babyPrefs, accessPrefs);
-    filtWindow.style.display = 'none';
-    map.setOptions({draggable: true});
-}
