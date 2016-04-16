f3f267f103b2fe820f7c92d271f99676ae8f798e
Lydia Watson <lydiawatson6@gmail.com>
2016-04-16 16:08:12 +1200

    tidying stage 2 of upload page, still using some global variables

--- www/js/upload.js
+++ www/js/upload.js
@@ -1,8 +1,5 @@
-"use strict";
-//create a reference to the database
-var firebaseRef = new Firebase("https://loocation.firebaseio.com/");
-//create a geoFire reference
-var geoFireRef = new GeoFire(firebaseRef.child("locations"));
+//var to determine whether marker location has been confirmed
+var confirmed = false;
 
 //declare a variable for the map
 var map;
@@ -10,94 +7,96 @@ var map;
 //declare variables for the entered marker and the marker's position
 var marker;
 var markerPos;
-var geocoder;
 
-//var to determine whether marker location has been confirmed
-var confirmed = false;
-//variables to reference the heading and confirm button in the HTML
-var topInstruction = document.getElementById('uploadHeading');
+ //variables to reference elements in the HTML
 var confirmLocation = document.getElementById('confirmButton');
+var uplWindow = document.getElementById('popUpUpload');
+var topInstruction = document.getElementById('uploadHeading');
 
+function runApp() {
+    "use strict";
+
+    //run relevant functions when various elements are clicked
+    document.getElementById("uploadCancel").onclick = endUpload;
+    confirmLocation.onclick = detailsForm;
+    document.getElementById("detailsCancel").onclick = unConfirmLocation;
+    document.getElementById('doneUpload').onclick = confirmAll;
+}
+
+
+
+//function to return the user to the homepage of the app, runs when upload is cancelled or completed
+/*needs nothing*/
 function endUpload() {
     window.open("index.html", "_self");
 }
-//???could do this without declaring the variable - it is not used ANYWHERE ELSE
-var cancelUpload = document.getElementById("uploadCancel");
-cancelUpload.onclick = endUpload;
+
 
 //function to open the map
+/*needs marker, map, markerPos and confirmed*/
 function initMap() {
-//  set up the map and give it an fallback center position
+    "use strict";
+    //set up the map and give it an fallback center position
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -36.8436, lng: 174.7669},
         zoom: 15,
-//        disable default controls and properties of the map
-//        ???consider disabling all default controls and then enabling what you want (currently none) - might be more efficient?
+        //disable default controls and properties of the map
         streetViewControl: false,
         mapTypeControl: false,
         zoomControl: false,
-//        zoomControlOptions: {
-//            position: google.maps.ControlPosition.TOP_LEFT
-//        }
         disableDoubleClickZoom: true
-//        ???check with others about turning off POIs
-//        styles: [{featureType: "poi", elementType: "labels", stylers: [{visibility: "off"}]}]
     });
-    
-//    if geolocation is working change the center of the map to the user's location
+
+    //if geolocation is working center the map on the user's position
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
             map.setCenter(initialLocation);
         });
     }
-    
+
     marker = new google.maps.Marker({});
-//    listener that adds a marker to the marker variable
+    
+    // add a listener that adds a marker to the map when it is clicked and find the marker's address
     map.addListener('click', function (e) {
         if (!confirmed) {
-//            markerPos = {lat: e.latLng.lat(), lng: e.latLng.lng()};
             marker.setPosition({lat: e.latLng.lat(), lng: e.latLng.lng()});
             marker.setMap(map);
             marker.setDraggable(true);
             topInstruction.innerHTML = "Confirm the marker position or modify";
             confirmLocation.style.display = "block";
             markerPos = marker.position;
-//            console.log(marker.position);
-//            console.log(marker.position.lat());
             findAddress();
         }
-        
+
     });
-    marker.addListener('dragend',function(event) {
-        alert("seven potatoes");
-                markerPos = marker.position;
+    
+    //change the variable containing the marker's position when it is dragged and update the address
+    marker.addListener('dragend', function(event) {     
+        markerPos = marker.position;
         findAddress();
-//            console.log(markerPos);
     });
-
 }
 
-
-var UplWindow = document.getElementById('popUpUpload');
-
+//function to work out the formatted address of the marker
+/*needs marker Pos and that is it*/
 function findAddress() {
-    geocoder = new google.maps.Geocoder();
+    //find the address using google's Geocoder service
+    var geocoder = new google.maps.Geocoder();
     geocoder.geocode({'location': markerPos}, function (results, status) {
-        
-//        gives full address:
-        
+        //gives full address:
         if (status === google.maps.GeocoderStatus.OK) {
+            //changes the pop-up window's content so that it displays the address
             document.getElementById("address").innerHTML = results[0].formatted_address;
-        } //consider having error messages here but otherwise nah
-        
-        
-
+        }
     });
 }
 
+//function to open the pop-up window for finishing the upload
+/*needs the uplWindow, topInstruction, confirmed, marker and map
+could get the first two just straight from the document with getElementById*/
 function detailsForm() {
-    UplWindow.style.display = "block";
+    uplWindow.style.display = "block";
     topInstruction.innerHTML = "Now enter the details...";
     confirmed = true;
     marker.setDraggable(false);
@@ -106,114 +105,50 @@ function detailsForm() {
     
     
 }
-confirmLocation.onclick = detailsForm;
 
 
+//function to allow the user to change the location of their marker after it has been confirmed
+/*needs confirmed, topInstruction, marker, map and upload window*/
 function unConfirmLocation() {
     confirmed = false;
-    //changing top instructions is only valid until user clicks somewhere else on the map, then it reverts to "confirm the marker position when done". Issue or no??
+    //changes top instruction telling the user to reconfirm the marker position
     topInstruction.innerHTML = "Reconfirm your marker position";
+    //allows the user to move the marker and map again
     marker.setDraggable(true);
     map.setOptions({draggable: true});
-    UplWindow.style.display = "none";
-    
+    uplWindow.style.display = "none";
 }
-//could do this without declaring the variable - it is not used ANYWHERE ELSE
-var cancelDetails = document.getElementById("detailsCancel");
-cancelDetails.onclick = unConfirmLocation;
 
-
+//function to save all of the information as a new entry in the database
+/*needs geoFireRef, firebaseRef, markerPos*/
 function confirmAll() {
+    // create references to the firebase database and to geofire
+    var firebaseRef = new Firebase("https://loocation.firebaseio.com/");
+    var geoFireRef = new GeoFire(firebaseRef.child("locations"));
+    
     //create an empty object to put the filter information into
     var looFilters = {};
-    //array containing the property categories for each filter
+    //array containing the property categories for each filter as they will be saved in the database
     var properties = ['male', 'fem', 'uni', 'mBab', 'fBab', 'uBab', 'wheelchair'];
     //array that holds the checkboxes and links to them in the HTML
     var values = ['maleUpload', 'femaleUpload', 'uniUpload', 'maleBabyUpload', 'femBabyUpload', 'uniBabyUpload', 'wheelchairUpload'];
-    //assign the filters to the looFilters object
+    //assign the filters to the looFilters object, declaring them either true or false depending on the user's input
     for (var i = 0; i < properties.length; i++) {
         values[i] = document.getElementById(values[i]);
         looFilters[properties[i]] = values[i].checked
     }
-    //add everything to firebase
-//    firebaseRef.push(
-//        {location: markerPos, filters: looFilters},
-//        function(error) {
-//            if (error) {
-//                alert('There has been some kind of dreadful problem.' + error);
-//            } else {
-//                alert('Your new Loocation has been saved successfully! Thank you for your contribution :)');
-//                endUpload();
-//            }
-//        }
-//    );
-    
-
-//    add everything to firebase using geofire for the location
-//    var filtersWorked = null;
-    
-//    var newUpload = firebaseRef.child("filters").push({filters: looFilters}).then(function() {
-//        geoFireRef.set(newUpload.key(), [markerPos.lat, markerPos.lng], function(error) {
-//            if (error) {
-//                alert('Unfortunately the location has not saved');
-//            }
-//        });
-//        alert("Your new Loocation has been saved successfully. Thank you for your contribution.");
-//        endUpload();
-//    }, function(error) {
-//            alert("Error: " + error);
-//        });
-//                
-//    }
-
-
-        geoFireRef.set(
-            firebaseRef.child("filters").push({looFilters}).key(), 
-            [markerPos.lat(), markerPos.lng()]).then(function() {
+    //upload the filter information to firebase under the filters child using a unique key generated by Firebase, then add the marker location using geoFire with the same key
+    geoFireRef.set(firebaseRef.child("filters").push({looFilters}).key(), [markerPos.lat(), markerPos.lng()]).then(function() {
             alert("Your new Loocation has been saved successfully. Thank you for your contribution.");
             endUpload();
         }, function(error) {
-            
-                alert('Unfortunately the location has not saved');
-            
-                
-            });
-        }
+            alert('Unfortunately the location has not saved');   
+        });
+}
 
         
     
 
-                
-    
-        
-//        
-//        function(error) {
-//            if (error) {
-//                alert('There has been some kind of dreadful problem.' + error);
-//            } else {
-//                filtersWorked = true;
-//                
-//            }
-//        }
-//    );
-//    
-//    geoFireRef.set(newUpload.key(), [markerPos.lat, markerPos.lng]).then(function() {
-//        if (filtersWorked) {
-//  alert("Your new Loocation has been saved successfully. Thank you for your contribution.");
-//        endUpload();
-//        }
-//}, function(error) {
-//  console.log("Error: " + error);
-//});
-//    
-//   newUpload.key();
-    
-    
-    
-    
 
-//could do this without declaring the variable - it is not used ANYWHERE ELSE
-var confirmUpload = document.getElementById('doneUpload');
-confirmUpload.onclick = confirmAll;
 
 
