f800beef36082fa37d02ff12936c1f4085044b52
Lydia Watson <lydiawatson6@gmail.com>
2016-04-19 20:30:50 +1200

    stuff

--- www/js/upload.js
+++ www/js/upload.js
@@ -8,6 +8,8 @@ function initMap() {
     //set up some elements as variables
     var confirmLocButton = document.getElementById('confirmButton');
     
+    var geoLocationWorks;
+    
     //set up the map and give it a fallback center
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -36.86803405818809, lng: 174.75977897644043},
@@ -27,16 +29,20 @@ function initMap() {
     //recenter the map on the user's position if possible
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
+            geoLocationWorks = true;
             var initialLocation = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
             map.setCenter(initialLocation);
+            allowMarkerEditing(map, marker, confirmLocButton, geoLocationWorks);
+            
         });
     }
+    console.log(geoLocationWorks);
     
     //allow the marker the be added to the map with any of the four methods of click, drag, search, or use user's location
-    allowMarkerEditing(map, marker, confirmLocButton);
+    allowMarkerEditing(map, marker, confirmLocButton, geoLocationWorks);
     
     
     //when cancel button is clicked return to home page
@@ -51,8 +57,9 @@ function endUpload() {
 }
 
 //function to allow the editing/addition of the marker to the map
-function allowMarkerEditing(map, marker, confirmLocButton) {
+function allowMarkerEditing(map, marker, confirmLocButton, geoLocationWorks) {
     "use strict";
+    console.log(geoLocationWorks);
     var input = document.getElementById('searchInput');
     marker.setDraggable(true);
     map.setOptions({draggable: true});
@@ -82,7 +89,8 @@ function allowMarkerEditing(map, marker, confirmLocButton) {
     });
     
     //set marker position by user's location if available
-    if (navigator.geolocation) {
+    if (geoLocationWorks) {
+        
         var myLocation = document.getElementById('myLoc');
         myLocation.style.display = 'block';
         myLocation.onclick = function () {
@@ -99,7 +107,7 @@ function allowMarkerEditing(map, marker, confirmLocButton) {
     }
     
     //when the confirm button is clicked, open the details form
-    confirmLocButton.onclick = function () {confirmMarker(map, marker, input, confirmLocButton); };
+    confirmLocButton.onclick = function () {confirmMarker(map, marker, input, confirmLocButton, geoLocationWorks); };
 }
 
 //function that runs when marker position is changed
@@ -128,7 +136,7 @@ function findAddress(markerPos) {
 }
 
 //function to open the pop-up window for finishing the upload and to prevent further editing of the marker
-function confirmMarker(map, marker, input, confirmLocButton) {
+function confirmMarker(map, marker, input, confirmLocButton, geoLocationWorks) {
     "use strict";
     //turn off all methods of editing or adding the marker
     marker.setDraggable(false);
@@ -136,7 +144,10 @@ function confirmMarker(map, marker, input, confirmLocButton) {
     google.maps.event.clearListeners(map, 'click');
     input.disabled = true;
     input.style.display = 'none';
-    document.getElementById('myLoc').style.display = 'none';
+    if (geoLocationWorks) {
+        document.getElementById('myLoc').style.display = 'none';   
+    }
+    
     
     //open the pop up and hide the confirm button
     document.getElementById('popUpUpload').style.display = 'block';
@@ -144,12 +155,12 @@ function confirmMarker(map, marker, input, confirmLocButton) {
     document.getElementById('uploadHeading').innerHTML = "Now enter the details...";
     
     //run functions when elements are clicked
-    document.getElementById('detailsCancel').onclick = function () {unConfirmMarker(map, marker, confirmLocButton); };
+    document.getElementById('detailsCancel').onclick = function () {unConfirmMarker(map, marker, confirmLocButton, geoLocationWorks); };
     document.getElementById('doneUpload').onclick = function () {confirmAll(marker); };
 }
 
 //function to allow the user to unconfirm marker location and change it
-function unConfirmMarker(map, marker, confirmLocButton) {
+function unConfirmMarker(map, marker, confirmLocButton, geoLocationWorks) {
     "use strict";
     //close the popUp and show the confirm button
     document.getElementById('popUpUpload').style.display = 'none';
@@ -157,7 +168,7 @@ function unConfirmMarker(map, marker, confirmLocButton) {
     document.getElementById('uploadHeading').innerHTML = 'Confirm marker position or modify';
     
     //turns all methods of editing the marker back on
-    allowMarkerEditing(map, marker, confirmLocButton);
+    allowMarkerEditing(map, marker, confirmLocButton, geoLocationWorks);
 }
 
 //function to save all of the information as a new entry in the database
